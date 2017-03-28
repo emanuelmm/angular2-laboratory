e8b5bd824293086480a2a5030c1750478330fb15
Emanuel Maia Cordeiro <emanuel@cialne.com.br>
2017-03-28 09:50:23 -0300

    adding servicos

---
+++ servicos/src/app/cursos/cursos.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CursosService } from '../cursos/cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  cursos: string[] = [];
+  cursosService: CursosService;
+  
+  constructor(_cursosService: CursosService) { 
+    //this.cursosService = new CursosService();
+    this.cursosService = _cursosService;
+  }
+
+  ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
+  }
+
+}
---
+++ servicos/src/app/cursos/cursos.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CursosService {
+
+    constructor() {
+        console.log('CursosService');
+    }
+
+    getCursos() {
+        return ['Angular 2', 'Java', 'Pnonegap'];
+    }
+
+}
